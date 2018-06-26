a7bd46176b58426222fb8d1486cde12252daa7b1
Yanny Castrillon <yannyandres117@gmail.com>
2018-06-25 23:15:24 -0700

    Add Httparty gem. Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'search_vehicles/search'
+
+  root to: 'vehicles#home'
+
+  get '/vehicles/search', to: 'search_vehicles#search_price'
+
   resources :vehicles
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
