eaffc5b5e8b2ca881d4d0ac44d1aac4e278d9520
Yanny Castrillon <yannyandres117@gmail.com>
2018-06-25 23:51:18 -0700

    Add parameters to the session hash to be able to go back to the search page.

--- app/controllers/search_vehicles_controller.rb
+++ app/controllers/search_vehicles_controller.rb
@@ -2,6 +2,8 @@ class SearchVehiclesController < ApplicationController
   extend VehicleService
 
   def search_price
+    session[:min] = params[:min]
+    session[:max] = params[:max]
     @search_vehicles = VehicleService.search_min_max_price(search_params)
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
   get 'search_vehicles/search'
-
   root to: 'vehicles#home'
-
-  get '/vehicles/search', to: 'search_vehicles#search_price'
-
+  get '/vehicles/search', to: 'search_vehicles#search_price', as: :search_price
   resources :vehicles
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
